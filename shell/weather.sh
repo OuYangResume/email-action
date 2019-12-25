f5f0501a6ad53525357f43007dd0dac4e0b17200
root <812634676@qq.com>
2019-12-25 21:11:41 +0800

    shell

---
+++ shell/weather.sh
@@ -0,0 +1,41 @@
+###
+ # @Author: oouyang
+ # @Date: 2019-12-25 17:05:25
+ # @LastEditTime : 2019-12-25 21:05:15
+ # @LastEditors  : Please set LastEditors
+ # @Description: 获取各个城市的天气并生成对应城市的文件
+ # @FilePath: /email-action/weather.sh
+ ###
+#!/bin/sh
+
+set -eux
+
+# 城市列表
+CITY=(ShenZhen WuHan YiChun)
+# 语言
+LANGUAGE="zh-CN"
+# 单位
+UNIT=m 
+UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
+
+# curl \
+#   -H "Accept-Language: $LANGUAGE" \
+#   -H "User-Agent: $UA" \
+#   -o ${CITY[0]}.html \
+#    wttr.in/${CITY[0]}?format=4\&$UNIT
+
+# curl \
+#   -H "Accept-Language: $LANGUAGE" \
+#   -H "User-Agent: $UA" \
+#   -o ${CITY[1]}.html \
+#    wttr.in/${CITY[1]}?format=4\&$UNIT
+
+
+for item in ShenZhen WuHan YiChun NanChang
+do
+    curl \
+      -H "Accept-Language: $LANGUAGE" \
+      -H "User-Agent: $UA" \
+      -o $item.html \
+       wttr.in/$item?format=4\&$UNIT
+done
--- weather.sh
+++ weather.sh
@@ -1,7 +1,7 @@
 ###
  # @Author: your name
  # @Date: 2019-12-25 17:05:25
- # @LastEditTime : 2019-12-25 18:00:02
+ # @LastEditTime : 2019-12-25 20:10:06
  # @LastEditors  : Please set LastEditors
  # @Description: In User Settings Edit
  # @FilePath: /email-action/weather.sh
@@ -10,7 +10,7 @@
 
 set -eux
 
-CITY={ShenZhen,YiChun,WuHan}
+CITY=ShenZhen
 LANGUAGE="zh-CN"
 UNIT=m
 UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
@@ -18,5 +18,5 @@ UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Ge
 curl \
   -H "Accept-Language: $LANGUAGE" \
   -H "User-Agent: $UA" \
-  -o result.html \
-  -s "wttr.in/$CITY?format=4\&$UNIT"
+  -o $CITY.html \
+   wttr.in/$CITY?format=4\&$UNIT
