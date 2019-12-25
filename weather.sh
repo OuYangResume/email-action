c4860dda3189301259f345be6fb18e90cfa5a8f6
root <812634676@qq.com>
2019-12-25 18:01:35 +0800

    添加城市

--- weather.sh
+++ weather.sh
@@ -1,7 +1,7 @@
 ###
  # @Author: your name
  # @Date: 2019-12-25 17:05:25
- # @LastEditTime : 2019-12-25 17:49:18
+ # @LastEditTime : 2019-12-25 18:00:02
  # @LastEditors  : Please set LastEditors
  # @Description: In User Settings Edit
  # @FilePath: /email-action/weather.sh
@@ -10,7 +10,7 @@
 
 set -eux
 
-CITY=ShenZhen
+CITY={ShenZhen,YiChun,WuHan}
 LANGUAGE="zh-CN"
 UNIT=m
 UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
@@ -19,4 +19,4 @@ curl \
   -H "Accept-Language: $LANGUAGE" \
   -H "User-Agent: $UA" \
   -o result.html \
-  wttr.in/$CITY?format=4\&$UNIT
+  -s "wttr.in/$CITY?format=4\&$UNIT"
